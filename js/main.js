7b475ccfe87d9bba9d99d8b1a7e8b66904cb418c
Ariadna Eyzaguirre <ariadna.eyzaguirre@gmail.com>
2016-10-10 15:06:42 -0500

    js arreglado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-var array=[];
 window.addEventListener("load",function(){
-    var mensaje = document.getElementById("mensaje");
     var chat = document.getElementById("chat");
     var conversacion = document.getElementById("conversacion");
     var mensajes = document.getElementById("mensajes");
@@ -19,25 +17,15 @@ window.addEventListener("load",function(){
     div2.appendChild(div3);    
     
     mensajes.addEventListener("keyup", function(e){
-        imprimirMensaje(e);
-        horaMensajes();
-        
+        var tecla = e.keyCode
+        if (tecla == 13){
+            imprimirMensaje(e);
+        }
     });
     
     function imprimirMensaje(e){
-        var tecla = e.keyCode;
-        var valorMensaje = mensajes.value;
-        div2.innerText = mensajes;
-        valorMensaje.insertBefore(div2);
-        horaMensajes();
-    }
-    function horaMensajes(){
-        var fecha = new Date();
-        var hora = fecha.getHours();
-        var min = fecha.getMinutes();
-        div3.innerText = hora + ":" + min;
-        mensajes.insertBefore(div3,mensajes.childNodes[0]);
-    }
-    
-    
+        var valorMensajes = mensajes.value;
+        div2.innerText = valorMensajes;
+        valorMensajes.insertBefore(div2);
+    }    
 });
