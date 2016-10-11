ead9e79028999aabb0e5deabec6283017486a225
Ariadna Eyzaguirre <ariadna.eyzaguirre@gmail.com>
2016-10-10 19:58:22 -0500

    nombre de div js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
 window.addEventListener("load",function(){
+    var mensaje = document.getElementById("mensaje");
+    var hora = document.getElementById("hora");
+    var chat = document.getElementById("chat");    
     var mensajes = document.getElementById("mensajes");
+    var conversacion = document.getElementById("conversacion");
     
-    function imprimirMensaje(e){
-        var conversacion = document.getElementById("conversacion");
-        var div1 = document.createElement("div");
-        var div2 = document.createElement("div");
-        var div3 = document.createElement("div");
-        var p = document.createElement("p");
+    function imprMensHora(e){
+        var divPadre = document.createElement("div");
+        var divHijo = document.createElement("div");
+        var divTiempo = document.createElement("div");
+        var pMensaje = document.createElement("p");
 
-        div1.classList.add("w-message","w-message-out");
-        div2.classList.add("w-message-text");
-        div3.classList.add("time");
+        divPadre.classList.add("w-message","w-message-out");
+        divHijo.classList.add("w-message-text");
+        divTiempo.classList.add("time");
 
         conversacion.appendChild(div1);
-        div1.appendChild(div2);
-        div2.appendChild(p);
-        div2.appendChild(div3);
+        divPadre.appendChild(divHijo);
+        divHijo.appendChild(pMensaje);
+        divHijo.appendChild(divTiempo);
         
         var valorMensajes = mensajes.value;
-        p.innerText = valorMensajes;
+        pMensaje.innerText = valorMensajes;
         
         var tiempo = new Date();
         var hora = tiempo.getHours();
         var minuto = tiempo.getMinutes();
         var horaMin = hora+":"+minuto;
-        div3.innerHTML = horaMin;
+        divTiempo.innerHTML = horaMin;
         conversacion.appendChild(tiempo);
     }
     
     mensajes.addEventListener("keyup", function(e){
         var tecla = e.keyCode
         if (tecla == 13){
-            imprimirMensaje(e);
+            imprMensHora(e);
         }
     });
     
